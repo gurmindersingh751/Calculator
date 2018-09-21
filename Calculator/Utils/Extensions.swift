93e4822c94cf28133b23daf190dce8ded77dbc6c
Gurminder singh <gurminder290195@gmail.com>
2018-09-21 01:51:30 -0400

    Added UIColor Extension to access colours in all the Controllers

---
+++ Calculator/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Calculator
+//
+//  Created by Gurminder Singh on 2018-09-21.
+//  Copyright © 2018 Gurminder Singh. All rights reserved.
+//
+
+import UIKit
+
+extension UiColor {
+    static func  rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
